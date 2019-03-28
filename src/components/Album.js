87b28547df706ce610f812aa6785b38630ae86c2
sandy-delarosa <sandy.delarosa97@gmail.com>
2019-03-27 20:36:22 -0400

    added song number and title and duration

--- src/components/Album.js
+++ src/components/Album.js
@@ -32,6 +32,16 @@ class Album extends Component {
 						<col id="song-duration-column"/>
 					</colgroup>
 					<tbody>
+						{
+							this.state.album.songs.map( (song, index) =>
+								<tr>
+									<td>{index + 1}</td>
+									<td>{song.title}</td>
+									<td>{song.duration} seconds</td>
+								</tr>
+							)
+
+						}
 					</tbody>
 				</table>
 			</section>
